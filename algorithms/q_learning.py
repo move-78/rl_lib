cb7657533d65b3015602ad8a9f8aba69be6ab9bd
move-78 <galstyan.artu@gmail.com>
2020-12-31 14:46:22 +0100

    first commit

---
+++ algorithms/q_learning.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class QLearning:
+    def __init__(self, n_states, n_actions, learning_rate, discount_factor):
+        assert 0 < learning_rate < 1, "Learning rate must be between 0 and 1."
+        assert 0 < discount_factor < 1, "Discount factor must be between 0 and 1."
+
+        self.n_states = n_states
+        self.n_actions = n_actions
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.q_table = np.zeros(shape=(n_states, n_actions))
+
+    def update(self, s_t, a_t, reward, s_next):
+        self.q_table[s_t, a_t] += self.learning_rate * self._get_temporal_difference(s_t, a_t, reward, s_next)
+
+    def _get_temporal_difference(self, s_t, a_t, reward, s_next):
+        return self._get_temporal_difference_target(reward, s_next) - self.q_table[s_t, a_t]
+
+    def _get_temporal_difference_target(self, reward, s_next):
+        return reward + self.discount_factor * np.max(self.q_table[s_next])
---
+++ main.py
@@ -0,0 +1,8 @@
+import gym
+import gym_gridworld
+
+if __name__ == "__main__":
+    env = gym.make("Gridworld-v0")
+    env.render()
+
+
